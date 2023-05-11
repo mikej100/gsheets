eacd34cc6cea5d8a9f46220bb7e74c3865095be1
Mike Jennings <mike.jennings@chwd.uk>
2023-05-12 00:11:11 +0100

    Add config.R to repo.

---
+++ RScripts/config.R
@@ -0,0 +1,5 @@
+cfg <- list(
+  gs4_auth_path = ".secrets/hja-001-api-key.json",
+  gsheet_url = "https://docs.google.com/spreadsheets/d/1o_m0_WYgxvar2eLgUlTYNHwLEjRTuBy0U_yd1X-eSrI/edit?resourcekey#gid=1954067065" 
+)
+
